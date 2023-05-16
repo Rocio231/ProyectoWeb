3d5f17c1fa1a772f0d0e9f8f06e645084e739ac1
Oscar Rojas <111471925+ElMysterSenior@users.noreply.github.com>
2023-05-16 14:25:47 -0600

    V3.5

--- webCenso/controlador/VerificarMunicipio.js
+++ webCenso/controlador/VerificarMunicipio.js
@@ -8,13 +8,11 @@ function recargarLista() {
     $.ajax({
         type: "POST",
         url: "../controlador/getLocalidades.php",
-        data: "id_municipio="+ $('#municipio').val(),
-        
-
+        data: "id_municipio=" + $('#municipio').val(),
         success: function(r) {
-
             $('#selectLocalidades').html(r);
+            $('#nombreLocalidad').val($('#selectLocalidades').val()); // agregar esta línea
         }
-
     });
 }
+
